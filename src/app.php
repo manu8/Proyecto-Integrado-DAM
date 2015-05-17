e4ed313502d0b09024c3b1045edea8d275e1e4ab
Manuel Berrio <manuelbm8@gmail.com>
2015-05-17 11:10:11 +0200

    Add Twig integration feature

--- src/app.php
+++ src/app.php
@@ -6,13 +6,25 @@ use Silex\Provider\UrlGeneratorServiceProvider;
 use Silex\Provider\ValidatorServiceProvider;
 use Silex\Provider\ServiceControllerServiceProvider;
 
+
+/**** INICIALIZACIÓN Y CONFIGURACIÓN DE LA APLICACIÓN ****/
+
 $app = new Application();
+$app['debug'] = true; //Activa la información de depuración
+
+
 $app->register(new UrlGeneratorServiceProvider());
 $app->register(new ValidatorServiceProvider());
 $app->register(new ServiceControllerServiceProvider());
-$app->register(new TwigServiceProvider());
-$app['twig'] = $app->share($app->extend('twig', function ($twig, $app) {
-    // add custom globals, filters, tags, ...
+
+//Integración de vistas con Twig
+$app->register(new TwigServiceProvider(), array(
+	'templates_path' => __DIR__.'../templates'
+));
+$app['twig'] = $app->share($app->extend('twig', function($twig, $app) {
+    $twig->addFunction(new \Twig_SimpleFunction('asset', function ($asset) {
+        return sprintf('../web/assets/%s', $asset);
+    }));
 
     return $twig;
 }));
