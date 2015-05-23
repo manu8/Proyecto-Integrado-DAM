42b21753b6ce72efb17f61a6ef47328bfb5a8731
Manuel Berrio <manuelbm8@gmail.com>
2015-05-23 16:08:42 +0200

    Doctrine and ORM Integration

--- src/app.php
+++ src/app.php
@@ -5,6 +5,8 @@ use Silex\Provider\TwigServiceProvider;
 use Silex\Provider\UrlGeneratorServiceProvider;
 use Silex\Provider\ValidatorServiceProvider;
 use Silex\Provider\ServiceControllerServiceProvider;
+use Silex\Provider\DoctrineServiceProvider;
+use Dflydev\Silex\Provider\DoctrineOrm\DoctrineOrmServiceProvider;
 
 
 /**** INICIALIZACIÓN Y CONFIGURACIÓN DE LA APLICACIÓN ****/
@@ -24,5 +26,25 @@ $app['twig'] = $app->share($app->extend('twig', function($twig) {
 
     return $twig;
 }));
+
+//Integración de la BD
+$app->register(new DoctrineServiceProvider(), array(
+    'db.options' => array(
+        'dbname' => 'curriculumsdb',
+        'password' => '',
+        'charset' => 'UTF-8'
+    ),
+));
+$app->register(new DoctrineOrmServiceProvider, array(
+    "orm.em.options" => array(
+        "mappings" => array(
+            array(
+                "type" => "annotation",
+                "namespace" => "Entities",
+                "path" => __DIR__."/src/Entities",
+            ),
+        ),
+    ),
+));
 
 return $app;
