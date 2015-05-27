e398f745efae77b54fe9096731e0737522cbf1d7
Manuel Berrio <manuelbm8@gmail.com>
2015-05-27 16:16:32 +0200

    Modifiying configuration files

--- config/prod.php
+++ config/prod.php
@@ -1,6 +1,12 @@
 <?php
 
-// configure your app for the production environment
+//Configuración de Twig
+$app['twig'] = $app->share($app->extend('twig', function($twig) {
+    $twig->addFunction(new \Twig_SimpleFunction('asset', function ($asset) {
+        return sprintf('../web/assets/%s', $asset);
+    }));
 
+    return $twig;
+}));
 $app['twig.path'] = array(__DIR__.'/../templates');
 $app['twig.options'] = array('cache' => __DIR__.'/../var/cache/twig');
