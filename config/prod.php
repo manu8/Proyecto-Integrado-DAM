96e4fc657e786c55da07d3c5d122d50a27e19a70
Manuel Berrio <manuelbm8@gmail.com>
2015-06-03 21:31:33 +0200

    Add users password encoder

--- config/prod.php
+++ config/prod.php
@@ -1,5 +1,8 @@
 <?php
 
+use Lib\Providers\UserProvider;
+use Symfony\Component\Security\Core\Encoder\MessageDigestPasswordEncoder;
+
 //Configuración de Twig
 $app['twig'] = $app->share($app->extend('twig', function($twig) {
     $twig->addFunction(new \Twig_SimpleFunction('asset', function ($asset) {
@@ -10,3 +13,25 @@ $app['twig'] = $app->share($app->extend('twig', function($twig) {
 }));
 $app['twig.path'] = array(__DIR__.'/../templates');
 $app['twig.options'] = array('cache' => __DIR__.'/../var/cache/twig');
+
+//Configuración de seguridad
+$app['security.firewalls'] = array(
+    'dev' => array(
+        'pattern' => '^/(_(profiler|wdt)|css|images|js)/',
+        'security' => false
+    ),
+    'secured' => array(
+        'pattern' => '^/admin/',
+        'form' => array(
+            'login_path' => '/user/login',
+            'check_path' => '/admin/login_check'
+        ),
+        'users' => $app->share(function() use ($app) {
+            return new UserProvider($app);
+        }),
+        'logout' => array('logout_path' => '/admin/logout')
+    )
+);
+$app['security_encoder.digest'] = $app->share(function () {
+    return new MessageDigestPasswordEncoder('sha1', false, 4);
+});
--- src/app.php
+++ src/app.php
@@ -32,13 +32,16 @@ $app->register(new DoctrineServiceProvider(), array(
     ),
 ));
 $app->register(new DoctrineOrmServiceProvider, array(
-    "orm.em.options" => array(
-        "mappings" => array(
+    'orm.proxies_dir' => __DIR__.'/../vendor/doctrine/orm/lib/Doctrine/ORM/Proxy',
+    'orm.proxies_namespace' => 'DoctrineProxy',
+    'orm.auto_generate_proxies' => true,
+    'orm.em.options' => array(
+        'mappings' => array(
             array(
-                "type" => "annotation",
-                "namespace" => "Entities",
-                "path" => __DIR__."/entities",
-                "use_simple_annotation_reader" => false,
+                'type' => 'annotation',
+                'namespace' => 'Entities',
+                'path' => __DIR__.'/entities',
+                'use_simple_annotation_reader' => false
             ),
         ),
     ),
@@ -46,30 +49,7 @@ $app->register(new DoctrineOrmServiceProvider, array(
 
 //Implementación de seguridad
 $app->register(new Silex\Provider\SessionServiceProvider());
-$app->register(new SecurityServiceProvider(), array(
-    'security.firewalls' => array(
-        'dev' => array(
-            'pattern' => '^/(_(profiler|wdt)|css|images|js)/',
-            'security' => false
-        ),
-        'secured' => array(
-            'pattern' => '^/admin/',
-            'form' => array(
-                'login_path' => '/user/login',
-                'check_path' => '/admin/login_check'
-            ),
-            'users' => $app->share(function() use ($app) {
-                return new Lib\Providers\UserProvider($app);
-            }),
-            'logout' => array('logout_path' => '/admin/logout'),
-        )
-    ),
-    'security.encoders' => array('Entities\Usuario' => array(
-        'algorithm' => 'sha1',
-        'iterations' => 4,
-        'encode_as_base64' => false
-    ))
-));
+$app->register(new SecurityServiceProvider());
 
 //Implementación del servidor de correo para verificación de usuario
 $app->register(new SwiftmailerServiceProvider());
