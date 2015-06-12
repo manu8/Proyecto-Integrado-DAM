0e9ad94f333e046b4410f817b55c51168eb12e57
Manuel Berrio <manuelbm8@gmail.com>
2015-06-12 17:10:21 +0200

    Modifiying assets

--- web/assets/js/app.js
+++ web/assets/js/app.js
@@ -1,3 +1,10 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
-$(document).foundation();
+$(document).foundation({
+    abide: {
+        patterns: {
+            surnames: /^([a-zA-ZáÁéÉíÍóÓúÚ+\s+])+$/,
+            tlf: /^(\d{9})$/
+        }
+    }
+});
